a19a1f4f4a3dd9733ee6ce6ffd3736810797ae90
Ajo Alex <102503764+ajoneoito@users.noreply.github.com>
2023-07-10 20:52:16 +0530

    doc

--- src/app/cli/page.tsx
+++ src/app/cli/page.tsx
@@ -1,11 +1,19 @@
+import Counter from "@/components/Counter";
+import Feature from "@/components/Features";
 import React from "react";
 
 function Index() {
   return (
-    <div>
-      Index
-      <br />
-      ksbdks
+    <div className="container mx-auto">
+      <div className="text-6xl font-bold">Glim CLI</div>
+      <div className="mt-4">
+        Glim cli is the react native boilerplate that provide hassle-free
+        architecture for developing react native applications. It's an all in
+        one library kit for react native developers. Through glim cli we can
+        create components, screens, redux components etc.
+      </div>
+      <Feature />
+      <Counter />
     </div>
   );
 }
